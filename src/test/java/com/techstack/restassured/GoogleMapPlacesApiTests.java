b8adef71a84bceb722b8187d5657a00568b5a558
Karthikeyan Nithiyanandam <karthikeyan_ng@gmail.com>
2020-02-16 13:40:14 +0530

    Validate content type and navigate body content and assert the value

--- src/test/java/com/techstack/restassured/GoogleMapPlacesApiTests.java
+++ src/test/java/com/techstack/restassured/GoogleMapPlacesApiTests.java
@@ -1,10 +1,12 @@
 package com.techstack.restassured;
 
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 
 public class GoogleMapPlacesApiTests {
@@ -13,17 +15,25 @@ public class GoogleMapPlacesApiTests {
     @Test
     void simplePlacesLookup() {
 
+        //https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&key=AIzaSyCBsZ88adr5hIyelfJHyVbM2BBwR84TJYE
+
         RestAssured.baseURI = "https://maps.googleapis.com";
 
         given().
             param("location", "-33.8670522,151.1957362").
             param("radius", "500").
-            param("Key", "AIzaSyCBsZ88adr5hIyelfJHyVbM2BBwR84TJYE").
+            param("key", "AIzaSyCBsZ88adr5hIyelfJHyVbM2BBwR84TJYE").
         when().
             get("/maps/api/place/nearbysearch/json").
         then().
             assertThat().
-                statusCode(200);
+                statusCode(200).
+                and().
+                contentType(ContentType.JSON).
+                and().
+//                body("results[0].geometry.location.lat", equalTo(-33.8688197d))
+                body("results[0].name", equalTo("Sydney"))
+        ;
 
 
     }
