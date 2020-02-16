b9684e31b23d8fd09752374cfbcad7c2d4816c0b
Karthikeyan Nithiyanandam <karthikeyan_ng@gmail.com>
2020-02-16 15:02:53 +0530

    In this change, add a place using POST method and extract the response and get the placeId from the response. Next commit, I will do the DELETE place by using placeId.

--- src/test/java/com/techstack/restassured/GoogleMapPlacesApiTests.java
+++ src/test/java/com/techstack/restassured/GoogleMapPlacesApiTests.java
@@ -2,6 +2,8 @@ package com.techstack.restassured;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -77,5 +79,49 @@ public class GoogleMapPlacesApiTests {
         ;
     }
 
+    @DisplayName("Add a location to Google Places and Delete the same place by using PlaceId")
+    @Test
+    void addAPlaceAndDeleteAddedPlace_usingPostAndDelete() {
+
+        String addPlacePayLoad = "{"+
+                "\"location\": {"+
+                "\"lat\": -33.8669710,"+
+                "\"lng\": 151.1958750"+
+                "},"+
+                "\"accuracy\": 50,"+
+                "\"name\": \"Google Shoes!\","+
+                "\"phone_number\": \"(02) 9374 4000\","+
+                "\"address\": \"48 Pirrama Road, Pyrmont, NSW 2009, Australia\","+
+                "\"types\": [\"shoe_store\"],"+
+                "\"website\": \"http://www.google.com.au/\","+
+                "\"language\": \"en-AU\""+
+                "}";
+
+        RestAssured.baseURI = "http://216.10.245.166";
+
+        //1. Grab the response
+        Response response = given().
+                queryParam("key", "qaclick123").
+                body(addPlacePayLoad).
+                when().
+                    post("/maps/api/place/add/json").
+                then().
+                    assertThat().
+                        statusCode(200).
+                            and().
+                        contentType(ContentType.JSON).
+                            and().
+                        body("status", equalTo("OK")).
+                        extract().response();
+
+        //2. Grab the PlaceId from the response
+        String responseString = response.asString();
+        System.out.println(responseString);
+
+        JsonPath path = new JsonPath(responseString);
+        String placeId = path.get("place_id");
+        System.out.println(placeId);
+
+    }
 
 }
