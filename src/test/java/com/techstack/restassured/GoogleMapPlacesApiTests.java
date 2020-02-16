77d00df71974aa11f4407b0415963d3535b5dcc9
Karthikeyan Nithiyanandam <karthikeyan_ng@gmail.com>
2020-02-16 14:47:30 +0530

    Add a new place to Google Places API (Actual is deprecated.)

--- src/test/java/com/techstack/restassured/GoogleMapPlacesApiTests.java
+++ src/test/java/com/techstack/restassured/GoogleMapPlacesApiTests.java
@@ -11,9 +11,9 @@ import static org.hamcrest.Matchers.equalTo;
 
 public class GoogleMapPlacesApiTests {
 
-    @DisplayName("Find Location for the given radius")
+    @DisplayName("GET: Find Location for the given radius")
     @Test
-    void simplePlacesLookup() {
+    void simplePlacesLookup_usingGet() {
 
         //https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&key=AIzaSyCBsZ88adr5hIyelfJHyVbM2BBwR84TJYE
 
@@ -38,7 +38,44 @@ public class GoogleMapPlacesApiTests {
                 and().
                 header("Server", equalTo("scaffolding on HTTPServer2"))
         ;
-
-
     }
+
+    /**
+     * Add a location to Google Places. But, this end point is removed from Google Places API.
+     * Hence, I used the modified version as shown below
+     */
+    @DisplayName("Add a location to Google Places.")
+    @Test
+    void simplePlacesLookup_usingPost() {
+
+        RestAssured.baseURI = "http://216.10.245.166";
+
+        given().
+            queryParam("key", "qaclick123").
+            body("{"+
+                    "\"location\": {"+
+                    "\"lat\": -33.8669710,"+
+                    "\"lng\": 151.1958750"+
+                    "},"+
+                    "\"accuracy\": 50,"+
+                    "\"name\": \"Google Shoes!\","+
+                    "\"phone_number\": \"(02) 9374 4000\","+
+                    "\"address\": \"48 Pirrama Road, Pyrmont, NSW 2009, Australia\","+
+                    "\"types\": [\"shoe_store\"],"+
+                    "\"website\": \"http://www.google.com.au/\","+
+                    "\"language\": \"en-AU\""+
+                    "}").
+        when().
+            post("/maps/api/place/add/json").
+        then().
+            assertThat().
+                statusCode(200).
+                and().
+                contentType(ContentType.JSON).
+                and().
+                body("status", equalTo("OK"))
+        ;
+    }
+
+
 }
