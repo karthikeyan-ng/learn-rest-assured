204035962cebbf519c45589f58d6d99f03652b56
Karthikeyan Nithiyanandam <karthikeyan_ng@gmail.com>
2020-02-27 11:18:29 +0530

    Extract the JSON response and get the value using JsonPath and assert the same.

--- src/test/java/com/techstack/restassured/GoogleMapPlacesApiTests.java
+++ src/test/java/com/techstack/restassured/GoogleMapPlacesApiTests.java
@@ -38,7 +38,7 @@ public class GoogleMapPlacesApiTests {
 
         RestAssured.baseURI = properties.getProperty("HOST_URI");
 
-        given().
+        Response response = given().
             param("location", "-33.8670522,151.1957362").
             param("radius", "500").
             param("key", properties.getProperty("PLACES_API_KEY")).
@@ -49,14 +49,10 @@ public class GoogleMapPlacesApiTests {
                 statusCode(200).
                 and().
                 contentType(ContentType.JSON).
-                and().
-//                body("results[0].geometry.location.lat", equalTo(-33.8688197d))
-                body("results[0].name", equalTo("Sydney")).
-                and().
-                body("results[0].place_id", equalTo("ChIJP3Sa8ziYEmsRUKgyFmh9AQM")).
-                and().
-                header("Server", equalTo("scaffolding on HTTPServer2"))
-        ;
+                extract().response();
+
+        JsonPath jsonPath = JsonUtils.getJsonPathFromResponse(response.asString());
+        assertEquals(Integer.valueOf(20), jsonPath.get("results.size()"));
     }
 
     /**
--- src/test/java/com/techstack/restassured/JsonUtils.java
+++ src/test/java/com/techstack/restassured/JsonUtils.java
@@ -1,5 +1,7 @@
 package com.techstack.restassured;
 
+import io.restassured.path.json.JsonPath;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -19,5 +21,18 @@ public class JsonUtils {
         Path resourceDirectory = Paths.get("src","test", "resources", "xml");
         Path filePath = Paths.get(resourceDirectory.toString(), fileName);
         return new String(Files.readAllBytes(filePath));
+    }
+
+    public static JsonPath getJsonPathFromResponse(String response) {
+
+        JsonPath jsonPath = new JsonPath(response);
+        return jsonPath;
+    }
+
+    public static String getValueFromJsonResponse(String response, String path) {
+
+        JsonPath jsonPath = new JsonPath(response);
+        String value = jsonPath.getString(path);
+        return value;
     }
 }
